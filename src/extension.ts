7fecb90a891f716773a9622c4bd56ac83efc156d
James Perih <james@hotdang.ca>
2024-07-29 16:46:51 -0600

    Works good now, with quote-encapsulated text; change order of replacement selection saving, which caused a glitch where the selection wouldn't be replaced.

--- src/extension.ts
+++ src/extension.ts
@@ -12,13 +12,8 @@ const getArbPath = (): string | undefined => {
   return path.join(vscode.workspace.workspaceFolders![0].uri.path, 'lib/src/l10n/arb/app_en.arb');
 };
 
-// const ARB_FILE_PATH = path.join(
-//   vscode.workspace.rootPath!,
-//   'lib/src/l10n/arb/app_en.arb'
-// );
-
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+  let modifyArbDisposable = vscode.commands.registerCommand(
     'extension.modifyArb',
     () => {
       if (!vscode.workspace.workspaceFolders) {
@@ -47,7 +42,109 @@ export function activate(context: vscode.ExtensionContext) {
         });
     }
   );
-  context.subscriptions.push(disposable);
+
+  let addArbDisposable = vscode.commands.registerCommand("extension.addToArb", () => {
+    // Get text selection
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    
+    let selection = editor.selection;
+    // if the selection doesn't start and end with ' or ",
+    // but they are adjacent to the selection, modify the
+    // selection to include them
+    let start = selection.start;
+    let end = selection.end;
+    
+    if (start.character > 0 && editor.document.getText(new vscode.Range(start.translate(0, -1), start)) === '"' || editor.document.getText(new vscode.Range(start.translate(0, -1), start)) === "'") {
+      start = start.translate(0, -1);
+    }
+    if (end.character < editor.document.lineAt(end.line).range.end.character && editor.document.getText(new vscode.Range(end, end.translate(0, 1))) === '"' || editor.document.getText(new vscode.Range(end, end.translate(0, 1))) === "'") {
+      end = end.translate(0, 1);
+    }
+
+    selection = new vscode.Selection(start, end);
+    let text = editor.document.getText(selection);
+
+    // remove leading and trailing quotes from text, though.
+    if (text.startsWith('"') || text.startsWith("'")) {
+      text = text.slice(1);
+    }
+
+    if (text.endsWith('"') || text.endsWith("'")) {
+      text = text.slice(0, -1);
+    }
+
+    // request a keyName
+    let keyName: String;
+    vscode.window.showInputBox({
+      prompt: 'Enter the key name',
+      title: 'key name',
+    }).then((keyName) => {
+      if (!keyName) {
+        vscode.window.showErrorMessage('You need to specify a');
+        return;
+      }
+      
+      // read in the ARB file 
+      fs.readFile(getArbPath()!, 'utf8', (err, data) => {
+        if (err) {
+          vscode.window.showErrorMessage('Failed to read ARB file');
+          return;
+        }
+    
+        let arbContent: any;
+        try {
+          arbContent = JSON.parse(data);
+        } catch (e) {
+          vscode.window.showErrorMessage('Failed to parse ARB file');
+          return
+        }
+
+        // ensure we don't have this key already
+        if (arbContent[keyName]) {
+          vscode.window.showErrorMessage('Key already exists');
+          
+          // replace selection with the value of this key
+          editor.edit((editBuilder) => {
+            editBuilder.replace(selection, `Get.context!.l10n.${keyName}`);
+          });
+
+          return;
+        }
+
+        // ensure we don't have this string already
+        for (const key in arbContent) {
+          let keyString = arbContent[key];
+
+          if (keyString.toLowerCase() === text.toLocaleLowerCase()) {
+            vscode.window.showErrorMessage(`String already exists with key: ${key}`);
+            // replace selection with the key we found
+            editor.edit((editBuilder) => {
+              editBuilder.replace(selection, `Get.context!.l10n.${key}`);
+            });
+            
+            return;
+          }
+        }
+
+        // create new key and string value to arb file
+        arbContent[keyName] = text;
+        // replace selection with the key we just created
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, `Get.context!.l10n.${keyName}`);
+        });
+
+        modifyArbFile(keyName, text);
+        modifyDartFile(keyName);
+        runFlutterGenL10n();        
+      });
+    });
+  });
+
+  context.subscriptions.push(modifyArbDisposable);
+  context.subscriptions.push(addArbDisposable);
 }
 
 function generateKey(variableName: string) {
@@ -101,11 +198,8 @@ function modifyArbFile(key: string, translation: string) {
       );
     } catch (e) {
       vscode.window.showErrorMessage('Failed to parse ARB file');
-      return
+      return;
     }
-
-
-    
   });
 }
 
